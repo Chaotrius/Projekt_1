17a73fb9bfe686c72c127e97444a8d6924d1b731
Luka <luka.karalic@htlstp.at>
2022-03-01 08:07:22 +0100

    master test

---
+++ src/at/ac/htlstp/sj21k4b/FileWriteTest.java
@@ -0,0 +1,12 @@
+package at.ac.htlstp.sj21k4b;
+
+import java.io.FileWriter;
+import java.io.FileWriter;
+
+public class FileWriteTest {
+
+    File file = new File("test.txt");
+    FileWriter fileWriter = new FileWriter(file);
+
+
+}
